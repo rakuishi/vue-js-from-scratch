f1817b60411858df1619f1cced09a90467bdd9f5
rakuishi <rakuishi@gmail.com>
2017-01-19 09:50:51 +0900

    Add chapter 5

---
+++ 5/src/page/Add.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <editor-view @add="add"></editor-view>
+  </div>
+</template>
+
+<script>
+import EditorView from '../components/EditorView'
+export default {
+  methods: {
+    add(data) {
+      this.$emit('add', data)
+      // 追加処理のあとに`/items`にナビゲーションする
+      this.$router.push({name: 'items'})
+    }
+  },
+  components: {
+    EditorView
+  }
+}
+</script>
